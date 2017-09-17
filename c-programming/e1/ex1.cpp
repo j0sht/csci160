e5bb70e14d90db4f551f270dd928c9eb35371ea9
Joshua Tate <jmtate@icloud.com>
2017-09-17 14:12:26 -0700

    Did extra credit assignment for e1

--- c-programming/e1/ex1.cpp
+++ c-programming/e1/ex1.cpp
@@ -8,5 +8,11 @@ int main(int argc, char *argv[])
   // This is also a comment
   printf("You are %d miles away.\n", distance);
 
+  // Extra credit: Print off five more lines of text
+  printf("This is line 1\n");
+  printf("This is line 2\n");
+  printf("This is line 3\n");
+  printf("This is line 4\n");
+  printf("This is line 5\n");
   return 0;
 }
