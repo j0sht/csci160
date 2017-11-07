7716407bc9c141a0a6dc81c6048ede60a735dc3f
Joshua Tate <jmtate@icloud.com>
2017-11-07 13:19:18 -0800

    Added example from C Primer Plus, Chapter 1

---
+++ c-programming/primer/c01/dogs.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int dogs;
+
+    printf("How many dogs do you have?\n");
+    scanf("%d", &dogs);
+    printf("So you have %d dog(s)!\n", dogs);
+
+    return 0;
+}
